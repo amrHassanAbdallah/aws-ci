f49f65fd0185f01413fe2286539040c7f108c327
Amr Hassan <amrh434@gmail.com>
2021-11-29 16:16:44 +0200

    chore : updated the config.

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,9 +1,3 @@
-if [[ -z "${S3_BUCKET_NAME}" ]]; then
-  MY_SCRIPT_VARIABLE="Some default value because DEPLOY_ENV is undefined"
-else
-  MY_SCRIPT_VARIABLE="${S3_BUCKET_NAME}"
-fi
 
-echo "'${S3_BUCKET_NAME:-default_value}' '${S3_BUCKET_NAME-default_value}' ${MY_SCRIPT_VARIABLE}"
 
-aws s3 cp --recursive --acl public-read ./build MY_SCRIPT_VARIABLE
+aws s3 cp --recursive --acl public-read ./build s3://ud-fe
